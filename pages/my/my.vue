a6a9175ef8d825b33110de7bd1010b4d63f43208
caiguina <caiguina@tsshine.com>
2021-07-17 18:35:14 +0800

    添加我的

--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,9 +1,159 @@
 <template>
-	<view class="">my</view>
+	<view>
+		<!-- 头部 -->
+		<view class="position-relative d-flex a-center" style="height: 320rpx;">
+			<view
+				class="iconfont icon-xiaoxi1 position-absolute text-white"
+				style="z-index: 100;font-size: 50rpx;top: 70rpx;right: 20rpx;"
+			></view>
+			<image src="/static/images/my/bg.jpg" style="height: 320rpx;width: 100%;"></image>
+			<view class="d-flex a-center position-absolute left-0 right-0" style="bottom: 50rpx;">
+				<image
+					src="/static/images/index/contentImag/demo6.jpg"
+					style="width: 145rpx;height: 145rpx;border: 5rpx solid #f8f9fa;"
+					class="rounded-circle ml-4"
+				></image>
+				<view class="ml-2 text-white font-md">关关雎鸠</view>
+				<view
+					class="all-flex-row a-self-end ml-auto px-2"
+					style="height: 70rpx;background: #FFD43F;color: #CC4A00;border-top-left-radius: 40rpx;border-bottom-left-radius: 40rpx;"
+				>
+					<view class="iconfont icon-huangguan line-h mr-2"></view>
+					会员积分 1.99
+				</view>
+			</view>
+		</view>
+
+		<!-- 图片分类 -->
+		<card>
+			<view slot="title" class="d-flex a-center j-sb">
+				<text class="font-md font-weight">我的订单</text>
+				<view class="text-secondary font">
+					全部订单
+					<text class="iconfont icon-you font"></text>
+				</view>
+			</view>
+			<view class="d-flex a-center">
+				<view
+					class="flex-1 all-flex-column py-3"
+					hover-class="bg-linght-muted"
+					v-for="(item, index) in typeIcon"
+					:key="index"
+				>
+					<view class="iconfont font-lg line-h" :class="item.iconName"></view>
+					<view>{{ item.title }}</view>
+				</view>
+			</view>
+		</card>
+
+		<view class="cutLine"></view>
+
+		<uni-list-item
+			v-for="(item, index) in listData"
+			:key="index"
+			:show-extra-icon="true"
+			:extra-icon="item.extraIcon"
+			showArrow
+			:title="item.title"
+			@click="navigate('user-set')"
+		/>
+
+		<view class="cutLine"></view>
+
+		<uni-list-item
+			:show-extra-icon="true"
+			:extra-icon="setExtraIcon"
+			showArrow
+			title="更多设置"
+			:to="`/pages/user-set/user-set`"
+		></uni-list-item>
+	</view>
 </template>
 
 <script>
+import card from '@/components/common/card.vue';
+import uniListItem from '@/components/uni-ui/uni-list-item/uni-list-item.vue';
+export default {
+	components: {
+		card,
+		uniListItem
+	},
+
+	data() {
+		return {
+			typeIcon: [
+				{
+					title: '待付款',
+					iconName: 'icon-4-145'
+				},
+				{
+					title: '待收货',
+					iconName: 'icon-daishouhuo'
+				},
+				{
+					title: '待评价',
+					iconName: 'icon-xiaoxi'
+				},
+				{
+					title: '退换修',
+					iconName: 'icon-buoumaotubiao46'
+				}
+			],
+
+			listData: [
+				{
+					title: '小米会员',
+					extraIcon: {
+						type: 'icon-VIP',
+						color: '#FDBF2E',
+						isUniIcon: false
+					}
+				},
+				{
+					title: '会员中心',
+					extraIcon: {
+						type: 'icon-huangguan',
+						color: '#FCBE2D',
+						isUniIcon: false
+					}
+				},
+				{
+					title: '服务中心',
+					extraIcon: {
+						type: 'icon-service',
+						color: '#FA6C5E',
+						isUniIcon: false
+					}
+				},
+				{
+					title: '小米之家',
+					extraIcon: {
+						type: 'icon-home',
+						color: '#FE8B42',
+						isUniIcon: false
+					}
+				},
+				{
+					title: '更多功能',
+					extraIcon: {
+						type: 'icon-gengduo',
+						color: '#9ED45A',
+						isUniIcon: false
+					}
+				}
+			],
+
+			setExtraIcon: {
+				type: 'icon-lujing',
+				color: '#808C98',
+				isUniIcon: false
+			}
+		};
+	},
+
+	methods: {
+	}
+};
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ pages/user-set/user-set.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="">
+		<card headTitle="账号管理">
+			<uni-list-item title="个人资料"></uni-list-item>
+			<uni-list-item title="收货地址"></uni-list-item>
+		</card>
+
+		<card headTitle="关于">
+			<uni-list-item title="个人资料"></uni-list-item>
+			<uni-list-item title="收货地址"></uni-list-item>
+		</card>
+
+		<view class="p-3"><button type="default" class="bg-white">退出登录</button></view>
+	</view>
+</template>
+
+<script>
+import card from '@/components/common/card.vue';
+import uniListItem from '@/components/uni-ui/uni-list-item/uni-list-item.vue';
+export default {
+	components: {
+		card,
+		uniListItem
+	}
+};
+</script>
+
+<style scoped>
+page {
+	background: #eeeeee;
+}
+</style>
